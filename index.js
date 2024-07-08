658dc4a85b0bdc7b2e9c2d0ac0c7b5615b7832b7
Yuriu Gavrikov <coolyuriu2012@gmail.com>
2024-07-08 15:10:35 +0300

    test calendar

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ let heart = document.querySelector(".strip__likes__heart");
 heart.onclick = () => {
   heart.classList.toggle("strip__likes__heart-liked");
 };
+
+let calendar = document.querySelector(".user-menu__calendar__btn");
+
+calendar.onclick = () => {
+  document.querySelector(".datepickr-calendar").classList.toggle("open");
+};
