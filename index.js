869b3cd211562f42364f08d76d83094b14564cee
Yuriu Gavrikov <coolyuriu2012@gmail.com>
2024-08-22 17:36:33 +0300

    test api

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ let heart = document.querySelector(".strip__likes__heart");
 heart.onclick = () => {
   heart.classList.toggle("strip__likes__heart-liked");
 };
-
+//--------------------------------------------------------------------------------------
 let calendar = document.querySelector(".user-menu__calendar__btn");
 
 calendar.onclick = () => {
   document.querySelector(".datepickr-calendar").classList.toggle("open");
 };
-
+//--------------------------------------------------------------------------------------
 let popUp = document.querySelector(".translation__proposal__title");
 
 popUp.onclick = () => {
@@ -17,20 +17,41 @@ popUp.onclick = () => {
     .querySelector(".translation__proposal__popup")
     .classList.toggle("open");
 };
+//--------------------------------------------------------------------------------------
+let stripMainImg = document.querySelector(".strip__main__img");
+
+const getImgRus = async () => {
+  await fetch("https://garfield-archive.ru/api.php?id=20181024")
+    .then((response) => response.json())
+    .then((json) => {
+      stripMainImg.style.backgroundImage = `url(${json.strip.rus_pic_url})`;
+    });
+};
+const getImgEng = async () => {
+  await fetch("https://garfield-archive.ru/api.php?id=20181024")
+    .then((response) => response.json())
+    .then((json) => {
+      stripMainImg.style.backgroundImage = `url(${json.strip.eng_pic_url})`;
+    });
+};
+getImgRus();
 
 let checkboxLanguage = document.querySelector(".checkbox__language");
 
 checkboxLanguage.addEventListener("change", function () {
-  console.log("qqq");
-  document
-    .querySelector(".strip__main__img")
-    .classList.toggle("strip__main__img__rus");
-  document
-    .querySelector(".strip__main__img")
-    .classList.toggle("strip__main__img__eng");
-});
+  const arrClasses = [];
+  stripMainImg.classList.forEach((item) => arrClasses.push(item));
 
-//qqq
-fetch("https://garfield-archive.ru/api.php?id=20240804")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+  console.log(arrClasses);
+  console.log(arrClasses.includes("strip__main__img__rus"));
+
+  if (arrClasses.includes("strip__main__img__rus")) {
+    getImgEng();
+  }
+  if (arrClasses.includes("strip__main__img__eng")) {
+    getImgRus();
+  }
+
+  stripMainImg.classList.toggle("strip__main__img__rus");
+  stripMainImg.classList.toggle("strip__main__img__eng");
+});
